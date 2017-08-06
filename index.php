e7fc86445ed8f997682b92d8b887e78d7b07e87b
steven <shebert2@cnm.edu>
2017-08-06 02:23:45 -0600

    fixed up header and toolbar realllnice

--- index.php
+++ index.php
@@ -30,8 +30,28 @@
 	</head>
 	<body>
 		<header>
-			<div class="container-fluid">
-			</div>
+			<nav class="navbar-collapsible-static">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
+								  aria-expanded="false" aria-controls="navbar">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">title</a>
+					</div>
+					<div id="navbar" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="url1">link1</a></li>
+							<li><a href="url1">link2</a></li>
+							<li><a href="url1">link3</a></li>
+							<li><a href="url1">link4</a></li>
+						</ul>
+					</div><!--/.nav-collapse -->
+				</div>
+			</nav>
 		</header>
 
 		<main>
