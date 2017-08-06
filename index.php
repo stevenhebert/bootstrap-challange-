ea535c5e3acc30d7d4c97bad7445e0feed2f9c08
steven <shebert2@cnm.edu>
2017-08-06 02:29:17 -0600

    fixed up header and toolbar realllnice

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
 	</head>
 	<body>
 		<header>
-			<nav class="navbar-collapsible-static">
+			<nav class="navbar navbar-default navbar-static-top">
 				<div class="container">
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
